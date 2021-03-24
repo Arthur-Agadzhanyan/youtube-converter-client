e211f67f73838f9fe4c9c537422b2ea3b2895aba
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2021-03-24 21:42:37 +0300

    create: добавлена возможность скачивания только аудио, только видео, сразу видео и аудио раздельно

--- src/components/YtdlForm/YtdlForm.tsx
+++ src/components/YtdlForm/YtdlForm.tsx
@@ -3,27 +3,49 @@ import React, { SyntheticEvent, useState } from 'react';
 const YtdlForm = () => {
     const [ytLink, setYtLink] = useState<string>('');
 
+    interface downloadButton {
+        text: string,
+        fetchUrl: string
+    }
 
-    const submitHandler = async (e: SyntheticEvent)=>{
+    const download = async (url: string) => {
+        if (ytLink) {
+            window.location.href = `http://localhost:5000/${url}?url=${ytLink}`
+            setYtLink('')
+        }
+    }
+
+    const downloadSplitedVideo = async (e: SyntheticEvent) => {
         e.preventDefault()
         if (ytLink) {
-            // const data = await fetch(`http://localhost:5000/download?url=${ytLink}`).then(data=>data.json())
-            // console.log(data);
-            
-            window.location.href = `http://localhost:5000/download?url=${ytLink}`
+            const videoUrl = `http://localhost:5000/download-video?url=${ytLink}`
+            const audioUrl = `http://localhost:5000/download-audio?url=${ytLink}`
+
+            window.location.href = videoUrl
+            setTimeout(() => window.location.href = audioUrl, 2000)
+
             setYtLink('')
-        }    
+        }
     }
 
-    const inputHandler = (e: React.ChangeEvent<HTMLInputElement>)=>{
+    const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setYtLink(e.target.value)
-    }
+    } 
+
+    const buttons: downloadButton[] = [
+        { text: 'Скачать', fetchUrl: 'download' },
+        { text: 'Скачать только аудио', fetchUrl: 'download-audio' },
+        { text: 'Скачать только видео', fetchUrl: 'download-video' }
+    ]
 
     return (
-        <form onSubmit={submitHandler}>
+        <>
             <input value={ytLink} type='text' onChange={inputHandler} />
-            <button type="submit">Convert</button>
-        </form>
+            {buttons.map((item, i) => {
+                return <button key={`${item}_${i}`} type="submit" onClick={() => download(item.fetchUrl)}>{item.text}</button>
+            })}
+            <button type="submit" onClick={downloadSplitedVideo}>Скачать поделённое видео</button>
+        </>
     );
 }
 
